75725f3fb2460ef1637fa2429b07afcb5063bc9f
Егор Сивцев <gumben7@gmail.com>
2022-12-16 11:44:27 +0700

    System added

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace OneThree {
     class Program {
         static void Main(string[] args) {
-            int n = Int32.Parse(Console.ReadLine());
+            int n = System.Int32.Parse(System.Console.ReadLine());
             bool[] a = new bool[n + 1];
             a[0] = true;
             a[1] = true;
-            double sqrN = Math.Sqrt(n);
+            double sqrN = System.Math.Sqrt(n);
             for (int i = 2; i <= sqrN; ++i) {
                 for (int s = i*i; s <= n; s += i) {
                     a[s] = true;                   
@@ -13,7 +13,7 @@
             }
             for(int i = 0; i <= n; ++i) {
                 if (!a[i]) {
-                    Console.Write(i + " ");
+                    System.Console.Write(i + " ");
                 }
             }
         }
